1fddaa22d04e8be312a31543bc802ca1b38e20d9
Carlos García <gacia.quintupil@gmail.com>
2025-06-28 09:01:55 -0400

    216. Rutas públicas

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -2,19 +2,40 @@ import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes";
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
 
+        {/** Distintas formas de hacerlo */}
+        {/** Primera forma: */}
+        <Route path="/login" element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>          
+        }/>
+        
+
+        {/** Segunda forma: */}
+        {/*
+        <Route path="/login/*" element={
+          <PublicRoute>
+            <Routes>
+              <Route path="/*" element={<LoginPage />} />
+            </Routes>
+          </PublicRoute>
+        }/>
+        */}
         <Route path="/*" element={
           <PrivateRoute>
             <HeroesRoutes />
           </PrivateRoute>
         }/>
+
+        {/** <Route path="login" element={<LoginPage />} /> */}
         {/** <Route path="/*" element={<HeroesRoutes />} /> */}
       </Routes>
     </>
