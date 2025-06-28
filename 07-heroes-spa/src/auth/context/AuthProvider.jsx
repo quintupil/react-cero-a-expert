aad1cf366bb7c7fadd662a70358a618df392cedb
Carlos García <gacia.quintupil@gmail.com>
2025-06-27 23:50:56 -0400

    213. Mantener usuario activo

--- 07-heroes-spa/src/auth/context/AuthProvider.jsx
+++ 07-heroes-spa/src/auth/context/AuthProvider.jsx
@@ -3,21 +3,37 @@ import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authReducer";
 import { types } from '../type/types';
 
-const initialState = {
+/*const initialState = {
     logged: false,
+}*/
+
+const init = () => {
+    const user = JSON.parse( localStorage.getItem('user') );
+
+    return {
+        logged: !!user,
+        user: user,
+    }
 }
+
 export const AuthProvider = ({ children }) => {
     
-    const [ authState, dispatch ] = useReducer( authReducer, initialState);
+    const [ authState, dispatch ] = useReducer( authReducer, {}, init);
 
     const login = ( name = '' ) => {
+        
+        const user = { id: 'ABC', name };
         const action = {
             type: types.login,
-            payload: {
+            payload: user,
+            /*payload: {
                 id: 'ABC',
                 name: name,
-            }
+            }*/
         }
+
+        localStorage.setItem('user', JSON.stringify(user));init
+
         dispatch( action );
     }
     
