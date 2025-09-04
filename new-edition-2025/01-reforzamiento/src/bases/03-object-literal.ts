2c3cbaf69d33fcf568fe1bfa7f9404768cf8b7b8
Carlos García <gacia.quintupil@gmail.com>
2025-09-03 23:08:30 -0400

    16. Interface de Typescript (New Edition 2025)

--- new-edition-2025/01-reforzamiento/src/bases/03-object-literal.ts
+++ new-edition-2025/01-reforzamiento/src/bases/03-object-literal.ts
@@ -1,8 +1,32 @@
-const ironman = {
+interface Person {
+    firstName: string;
+    lastName: string; 
+    age: number;
+    address?: Address; //? Propiedad opcional
+}
+
+interface Address {
+    postalCode: string;
+    city: string;
+}
+
+const ironman: Person = {
     firstName: 'Tony',
     lastName: 'Stark',  
     age: 45,
+    address: {
+        postalCode: 'ABC123',
+        city: 'New York',
+    }
 };
+
+const spiderman: Person = {
+    firstName: 'Peter',
+    lastName: 'Parker',
+    age: 21,
+};
+
+console.log({ironman, spiderman});
 
 //Esto no genera un clone del objeto, si no que crea una referencia al mismo espacio de memoria (dirección de memoria)
 //const spiderman = ironman;
@@ -10,10 +34,11 @@ const ironman = {
 //Simbolo Spreed (...) :- Toma las propiedades del objeto ironman y las asigna a un nuevo objeto (spiderman). Rompe la referencia a primer nivel
 //const spiderman = {...ironman};
 
+/* Important!!! 
 const spiderman = structuredClone(ironman); //Clonación profunda (deep clone)
-
 spiderman.firstName = 'Peter';
 spiderman.lastName = 'Parker';
 spiderman.age = 21;        
+*/
 
-console.log(ironman, spiderman);
+//console.log(ironman, spiderman);
