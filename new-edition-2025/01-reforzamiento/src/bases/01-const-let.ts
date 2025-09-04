9c559ee91d3792262edd3222bdd92a781c9bd798
Carlos García <gacia.quintupil@gmail.com>
2025-09-03 21:40:42 -0400

    13. Variables y constantes (New Edition 2025)

---
+++ new-edition-2025/01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,15 @@
+const firstName: string = 'Tony';
+const lastName = 'Stark';
+
+let diceNumber = 5;
+diceNumber = 3;
+
+//String.includes(searchString: string, position?: number): boolean
+const containsLetterS = lastName.includes('S');
+
+console.log({ containsLetterS, diceNumber, firstName, lastName });
+
+//No utilizar var !!!!
+
+
+
