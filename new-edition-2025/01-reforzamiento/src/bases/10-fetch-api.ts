a73fd62e9270cb9fc6bece676034a2261e62000a
Carlos García <gacia.quintupil@gmail.com>
2025-09-14 09:32:21 -0300

    29. Interfaces y optimización. (New Edition 2025)

--- new-edition-2025/01-reforzamiento/src/bases/10-fetch-api.ts
+++ new-edition-2025/01-reforzamiento/src/bases/10-fetch-api.ts
@@ -1,19 +1,20 @@
+import type { GiphyRandomResponse } from "./data/giphy.response";
+
 const API_KEY = 'FAiNQBWphR8NeK6iaTocWSfm69l9Xbid';
 const myRequest = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
 
+const createImageInsideDOM = (url: string) => {
+    const imgElement = document.createElement('img');
+    imgElement.src= url;
+    document.body.append(imgElement);
+}
+
 myRequest
     .then((response) => {
-        //console.log(response);
-        response.json().then((data) => { //promise en cadena 
-            //console.log(data);
-            const imageUrl = data.data.images.original.url;
-            //console.log(imageUrl);
-
-            const imgElement = document.createElement('img');
-            imgElement.src= imageUrl;
-
-            document.body.append(imgElement);
-
+        response.json().then(({data}: GiphyRandomResponse) => { //promise en cadena 
+            const imageUrl = data.images.original.url;
+            //const imageUrl = data.data.images.original.url;
+            createImageInsideDOM(imageUrl);
         });
     })
     .catch((err) => {
