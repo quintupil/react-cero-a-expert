44001c739243d948930e45ab858df815c641ade9
Carlos García <gacia.quintupil@gmail.com>
2025-09-15 17:59:01 -0300

    41. Componente - ItemCounter. (New Edition 2025)

--- new-edition-2025/02-first-steps/src/main.tsx
+++ new-edition-2025/02-first-steps/src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { FirstStepApp } from './FirstStepsApp'
-import { MyAwesomeApp } from './MyAwesomeApp'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { FirstStepApp } from "./FirstStepsApp";
+//import { MyAwesomeApp } from './MyAwesomeApp'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/*<FirstStepApp/>*/}
-    <MyAwesomeApp/>
+    <FirstStepApp />
+    {/*<MyAwesomeApp/>*/}
   </StrictMode>
-)
+);
